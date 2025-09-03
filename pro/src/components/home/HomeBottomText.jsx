e023d1504059c0e7a8b84041b0c72e4151b31898
Nitesh pandey <140426447+Niteshpandey80@users.noreply.github.com>
2025-09-03 11:03:47 +0530

    new change

--- pro/src/components/home/HomeBottomText.jsx
+++ pro/src/components/home/HomeBottomText.jsx
@@ -3,17 +3,24 @@ import { Link } from 'react-router-dom'
 
 const HomeBottomText = () => {
   return (
-    <div className="flex items-center justify-center gap-6 "> 
-      <Link 
-       className='text-[6.5vw] border-4 border-white rounded-full px-8 py-1 uppercase text-white'
-      >
-        Projets
-      </Link>
-      <Link 
-       className='text-[6.5vw] border-4 border-white rounded-full px-8 py-1 uppercase text-white'
-      >
-        Agence
-      </Link>
+    <div className="flex items-center justify-center gap-6 text-white">
+      <div className="border-4 h-44 flex items-center px-10 border-white rounded-full uppercase  ">
+        <Link 
+          className="text-[6vw] hover:text-[#D3FD50]  mt-6 text-white" 
+          to="/projects"
+        >
+          Projets
+        </Link>
+      </div>
+
+      <div className="border-4 h-44 flex items-center px-10 border-white rounded-full uppercase">
+        <Link 
+          className="text-[6vw] hover:text-[#D3FD50]  mt-6 text-white" 
+          to="/agence"
+        > 
+          Agence
+        </Link>
+      </div>
     </div>
   )
 }
--- pro/src/pages/Agence.jsx
+++ pro/src/pages/Agence.jsx
@@ -2,8 +2,17 @@ import React from 'react'
 
 const Agence = () => {
   return (
-    <div>
-      <h1> Agence</h1>
+    <div >
+      <div>
+        
+      </div>
+      <div className=' mt-[55vh]' >
+        <h1 className='text-[20vw] font-midum text-center uppercase leading-[16vw]'>Soixan7e <br />
+Douze</h1>
+      </div>
+      <div className='lg:pl-[40%] lg:mt-20 mt-4 p-3'>
+         <p className='lg:text-5xl text-xl leading-tight'>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Notre curiosité nourrit notre créativité. On reste humbles et on dit non aux gros egos, même le vôtre. Une marque est vivante. Elle a des valeurs, une personnalité, une histoire. Si on oublie ça, on peut faire de bons chiffres à court terme, mais on la tue à long terme. C’est pour ça qu’on s’engage à donner de la perspective, pour bâtir des marques influentes.</p>
+      </div>
     </div>
   )
 }
